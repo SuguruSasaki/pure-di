02ed93ffc17298f8b4b6dc4b1715e2d39b34ab72
sugurusasaki <ss0402@gmail.com>
2019-01-25 17:23:19 +0900

    refactor: comments

--- Pure/ViewModule.cs
+++ Pure/ViewModule.cs
@@ -7,20 +7,22 @@ namespace Pure
     public class ViewModel
     {
         /// <summary>
-        /// 依存オブジェクト
+        /// Dependency class
+        /// This class have dependency with ViewModel
         /// </summary>
         public abstract class Dependency { }
 
         /// <summary>
-        /// パラメータオブジェクト 
+        /// Payload parameter class.
+        /// This class have some parameters.
         /// </summary>
         public abstract class Payload { }
 
         /// <summary>
-        /// 依存性の注入、Pure DIは依存性とパラメータを同時に渡します。
+        /// Dependency injection, Pure DI passes dependencies and parameters at the same time.
         /// </summary>
-        /// <param name="dependency">依存オブジェクト</param>
-        /// <param name="payload">インスタンスに渡すパラメータ</param>
+        /// <param name="dependency">instance of Dependency</param>
+        /// <param name="payload">instance of Payload</param>
         public virtual void Inject(Dependency dependency, Payload payload) { }
     }
 }
